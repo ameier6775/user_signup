1ad2e66b1f9c3970313d34e4267d8bf57babf30c
Alex Meier <awmmcb@mail.missouri.edu>
2019-04-08 19:07:28 -0500

    Added the basic HTML along with verification

--- main.py
+++ main.py
@@ -1,50 +1,46 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 import cgi
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-
-def get_title():
-    question = input()
-    answer =
-
-
+app.config['DEBUG'] = True
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     new_account = request.form['new-account']
-
-    escaped_account = cig.escape(new_account)
-
-    if escaped_account == "":
-        error = 'Please enter a valid email'
-        return redirect('/?error='+ error)
-
-    if escaped_account.len() < 3 or escaped_account.len() > 20:
-        error_result = 'Please enter a valid email'
-        return redirect('/?error=' + error)
-
-    result = 'The user,' + escaped_account + ' has been added.'
-
-    return result
-
-@app.route('/save_password', method=['POST'])
-def add_pass():
     new_password = request.form['new-password']
+    password_ver = request.form['password-verified']
+    new_email = request.form['email-created']
 
+    escaped_account = cgi.escape(new_account)
     escaped_password = cgi.escape(new_password)
+    escaped_ver = cgi.escape(password_ver)
+    escaped_email = cgi.escape(new_email)
 
-    if escaped_password == "":
-        error = 'Please enter a pasword'
+
+    if escaped_account == "" or len(escaped_account) < 3 or len(escaped_account) > 20:
+        error = 'That\'s not a valid username'
         return redirect('/?error='+ error)
 
-    if escaped_password.len() < 3 or escaped_password.len() > 20:
-        error_result = 'Please enter a password'
+    if escaped_password == "" or len(escaped_password) < 3 or len(escaped_password) > 20:
+        error = 'That\'s not a valid password'
+        return redirect('/?error='+ error)
+
+    if escaped_password != escaped_ver:
+        error = 'Passwords don\'t match'
         return redirect('/?error=' + error)
+
+    if len(escaped_email) > 0:
+        if "@" not in escaped_email or "." not in escaped_email or len(escaped_email) < 3 or len(escaped_email) > 20:
+            error = 'That\'s not a valid email'
+            return redirect('/?error=' + error)
+        
+    return render_template('welcome.html', result=escaped_account)
 
 @app.route('/')
 def index():
     error_arg = request.args.get('error')
     
-    return render_primary('index.html', users=add_user())
+    return render_template('index.html', error=error_arg)
+
+app.run()
