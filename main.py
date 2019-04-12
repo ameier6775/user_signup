dfea7658f3339896d94d580ac022a5cef1dbcd84
Alex Meier <awmmcb@mail.missouri.edu>
2019-04-11 20:05:18 -0500

    Validated it for spaces

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ def add_user():
     email_error = ""
 
     errors = []
-    names = []
+    names = []    
 
-    if new_account == "" or len(new_account) < 3 or len(new_account) > 20:
+
+    if new_account == "" or len(new_account) < 3 or len(new_account) > 20 or " " in new_account:
         user_error = 'That\'s not a valid username'
         errors.append(user_error)
-        names.append(new_account)
+        new_account = ""
 
-    if new_password == "" or len(new_password) < 3 or len(new_password) > 20:
+    if new_password == "" or len(new_password) < 3 or len(new_password) > 20 or " " in new_password:
         password_error = 'That\'s not a valid password'
         errors.append(password_error)
 
@@ -35,11 +36,14 @@ def add_user():
         errors.append(pass_val_error)
 
 
-    if len(new_email) > 0:
+    if len(new_email) > 0 or " " in new_email:
         if "@" not in new_email or "." not in new_email or len(new_email) < 3 or len(new_email) > 20:
             email_error = 'That\'s not a valid email'
             errors.append(email_error)
-            names.append(new_email)
+            new_email = ""
+
+    names.append(new_email)
+    names.append(new_account)
 
     if len(errors) > 0:
         return render_template('index.html', user_error=user_error, password_error=password_error, pass_val_error=pass_val_error, email_error=email_error, new_account=new_account, new_email=new_email)
